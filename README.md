b7abde7a877021cc34d5c8b78459995c9ef9cbd9
tweety27 <ktjdus00@gmail.com>
2024-09-09 10:10:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# chooroom
---
+++ src/test/java/com/lgdx/chooroom/ChooroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgdx.chooroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

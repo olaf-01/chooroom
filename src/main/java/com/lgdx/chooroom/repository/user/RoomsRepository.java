44fde5447ea8a9cb219108c461f9562335509355
huunji <ansguswl9684@gmail.com>
2024-09-11 15:42:34 +0900

    userService 작성중 -현지

---
+++ src/main/java/com/lgdx/chooroom/repository/user/RoomsRepository.java
@@ -0,0 +1,9 @@
+package com.lgdx.chooroom.repository.user;
+
+import com.lgdx.chooroom.domain.room.Rooms;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomsRepository extends JpaRepository<Rooms, String> {
+}
---
+++ src/test/java/com/lgdx/chooroom/service/user/RoomServiceTest.java
@@ -0,0 +1,8 @@
+package com.lgdx.chooroom.service.user;
+
+import org.junit.jupiter.api.Test;
+
+@Test
+public class RoomServiceTest {
+
+}
